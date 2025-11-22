aa209c750db0d935200780bbdda633eea5d123a7
pararead143-ui <pararead143@gmail.com>
2025-11-23 02:58:59 +0800

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
 
 # Register your models here.
+# users/admin.py
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User  # Import from the users app, NOT materials
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ("id", "email", "username", "is_staff", "is_superuser", "date_joined")
+    list_filter = ("is_staff", "is_superuser", "is_active")
+    search_fields = ("email", "username")
+    ordering = ("id",)
+
+admin.site.register(User, UserAdmin)
